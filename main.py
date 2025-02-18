545a7ec0d170c6ee8ae9e188987472330a644713
Shinimaa <gariktop1gg@gmail.com>
2025-02-18 23:54:04 +0300

    Update main.py
    Не работают оба апи

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import sys
-from io import BytesIO  # Этот класс поможет нам сделать картинку из потока байт
-
+from io import BytesIO
 import requests
 from PIL import Image
+import pygame
 
 # python search.py Москва, ул. Ак. Королева, 12
-toponym_to_find = " ".join("Москва, ул. Ак. Королева, 12")
-
+toponym_to_find = "Москва, ул. Ак. Королева, 12"
 geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
 geocoder_params = {
-    "apikey": "901c2d74-0eb9-4ba0-b49d-6ba02a4aead5",
+    "apikey": "8013b162-6b42-4997-9691-77b7074026e0",
     "geocode": toponym_to_find,
     "format": "json"}
 
@@ -36,7 +35,7 @@ def create_flag(ll):
     return f"{ll},pm2bl"
 
 
-apikey = "901c2d74-0eb9-4ba0-b49d-6ba02a4aead5"
+apikey = "f3a0fe3a-b07e-4840-a1da-06f18b2ddf13"
 ll, spn = get_map_parameters()
 map_params = {
     "ll": ll,
@@ -44,10 +43,24 @@ map_params = {
     "l": "map",
     "pt": create_flag(ll),
     "apikey": apikey,
-
 }
 map_api_server = "https://static-maps.yandex.ru/v1"
 response = requests.get(map_api_server, params=map_params)
+pygame.init()
 im = BytesIO(response.content)
 opened_image = Image.open(im)
-opened_image.show()
+width, height = opened_image.size
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Map")
+mode = opened_image.mode
+size = opened_image.size
+data = opened_image.tobytes()
+map_surface = pygame.image.fromstring(data, size, mode)
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.blit(map_surface, (0, 0))
+    pygame.display.flip()
+pygame.quit()
